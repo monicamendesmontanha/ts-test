e8e9740acb45e36dbb360a883eba0530420ed9d4
Monica Mendes <monicamendesmontanha@gmail.com>
2024-05-09 16:14:42 +1000

    Implement getArnieQuotes to perform requests in parallel

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,44 @@
-import { httpGet } from './mock-http-interface';
+import { httpGet } from "./mock-http-interface";
 
-// TODO define this type properly
-type TResult = any;
+type TResult = {
+  [key: string]: string;
+};
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+type TIndexedArnieQuote = {
+  index: number;
+  result: TResult;
+};
+
+const fetchArnieQuote = async (
+  url: string,
+  index: number
+): Promise<TIndexedArnieQuote> => {
+  const response = await httpGet(url);
+  const jsonResponse = JSON.parse(response.body);
+  const message = jsonResponse.message;
+
+  if (response.status === 200) {
+    return { index, result: { "Arnie Quote": message } };
+  }
+  return { index, result: { FAILURE: message } };
+};
+
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+  const pendingIndexedRequests: Promise<TIndexedArnieQuote>[] = urls.map(
+    (url: string, index: number) => {
+      return fetchArnieQuote(url, index);
+    }
+  );
+
+  const arnieQuotesResults: TIndexedArnieQuote[] = await Promise.all(
+    pendingIndexedRequests
+  );
+
+  const results: TResult[] = arnieQuotesResults
+    .sort((a: TIndexedArnieQuote, b: TIndexedArnieQuote) => a.index - b.index)
+    .map((quote: TIndexedArnieQuote): TResult => {
+      return quote.result;
+    });
+
+  return results;
 };
